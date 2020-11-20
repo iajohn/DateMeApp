0f70f79a8035beff7ad919c07bf63804ccc001b6
iajohn <ipheholuwahardewunmi@gmail.com>
2020-11-20 17:26:45 +0100

    Api and Client Skeleton Commit

--- API/Startup.cs
+++ API/Startup.cs
@@ -40,6 +40,7 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +57,11 @@ namespace API
 
             app.UseRouting();
 
+            app.UseCors(policy => policy.AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()
+                .WithOrigins("https://localhost:4200"));
+                
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
